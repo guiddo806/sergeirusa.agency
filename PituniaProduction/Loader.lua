8304a2bb4e6968c59c44e32a3f278c74ec02393c
guiddo806 <korovkotymur@gmail.com>
2024-10-24 00:45:45 +0300

    Delete PituniaProduction/Loader.lua

--- PituniaProduction/Loader.lua
+++
@@ -1,7 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/guiddo806/sergeirusa.agency/refs/heads/main/UI/ModdedLinoria.lua"))()
-
-if game.PlaceId == 13253735473 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/guiddo806/sergeirusa.agency/refs/heads/main/PituniaProduction/feofelact.TridentSurvival.lua",true))()
-elseif game.PlaceId == 301549746 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/guiddo806/sergeirusa.agency/refs/heads/main/PituniaProduction/feofelact.CounterBlox.lua",true))()
-
