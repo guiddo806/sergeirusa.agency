55ef6376c5bbc114e45cd6828f7e945565ad9074
guiddo806 <korovkotymur@gmail.com>
2025-02-14 04:06:40 +0200

    Update mvb.lua

--- PituniaProduction/mvb.lua
+++ PituniaProduction/mvb.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://raw.githubusercontent.com/tamerlan806/bit.hub/refs/heads/main/main/loader.lua", true))()
+loadstring(game:HttpGet("http://raw.githubusercontent.com/tamerlan806/bit.hub/refs/heads/main/main/crosshair.lua", true))()
