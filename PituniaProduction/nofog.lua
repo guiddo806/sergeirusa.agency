f71d2110d60f1c816aa021b148a04723744b813e
guiddo806 <korovkotymur@gmail.com>
2025-02-28 01:44:14 +0200

    Delete PituniaProduction/nofog.lua

--- PituniaProduction/nofog.lua
+++
@@ -1,7 +0,0 @@
-pcall(function()
-    local lighting = game:GetService("Lighting")
-    lighting.FogEnd = 1e10
-    lighting.Changed:Connect(function()
-        lighting.FogEnd = 1e10
-    end)
-end)
