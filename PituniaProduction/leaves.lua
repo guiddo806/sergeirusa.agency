6b26bf40c6703db5e031eebcb7f10b46dbdcdbdf
guiddo806 <korovkotymur@gmail.com>
2025-03-08 10:19:09 +0200

    Update leaves.lua

--- PituniaProduction/leaves.lua
+++ PituniaProduction/leaves.lua
@@ -1,22 +1,29 @@
-local Workspace = game:GetService("Workspace")
+local workspace = game:GetService("Workspace")
+local leaves = {"Fir3_Leaves", "Birch1_Leaves", "Elm1_Leaves", "Palm1_Leaves"}
 
-local leavesToDelete = {
-    "Fir3_Leaves",
-    "Birch1_Leaves",
-    "Elm1_Leaves",
-    "Palm1_Leaves"
-}
+local oldNamecall
+oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
+    local method = getnamecallmethod()
+    if self == workspace and method == "DescendantAdded" then
+        return oldNamecall(self, ...) 
+    end
+    return oldNamecall(self, ...)
+end)
 
-local function checkAndDeleteLeaves(obj)
-    if table.find(leavesToDelete, obj.Name) then
-        obj:Destroy()
+local function spoofLeaves(obj)
+    if table.find(leaves, obj.Name) then
+        obj.Transparency = 1
+        obj.CanCollide = false
+        if obj:IsA("BasePart") then
+            obj.CastShadow = false
+        end
     end
 end
 
-for _, obj in pairs(Workspace:GetDescendants()) do
-    checkAndDeleteLeaves(obj)
+for _, obj in pairs(workspace:GetDescendants()) do
+    spoofLeaves(obj)
 end
 
-Workspace.DescendantAdded:Connect(function(obj)
-    checkAndDeleteLeaves(obj)
+workspace.DescendantAdded:Connect(function(obj)
+    spoofLeaves(obj)
 end)
